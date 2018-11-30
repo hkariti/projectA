2e504bdb8bdc14eed05fb5eac3eaf81c6137cf9c
Hagai Kariti <hkariti@gmail.com>
2018-11-30 17:01:30 +0200

    add -y to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,4 +12,4 @@ EOF
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D4284CDD
 echo "deb https://repo.iovisor.org/apt/bionic bionic main" | sudo tee /etc/apt/sources.list.d/iovisor.list
 sudo apt-get update
-sudo apt-get install bcc-tools libbcc-examples linux-headers-$(uname -r) python-pip
+sudo apt-get install -y bcc-tools libbcc-examples linux-headers-$(uname -r) python-pip
