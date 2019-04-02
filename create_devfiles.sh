351ec98f0a94987f77cb6f16b03265d375a37a8d
Hagai Kariti <hkariti@gmail.com>
2019-04-02 10:15:49 +0300

    remove block_trace from create_devfiles

--- create_devfiles.sh
+++ create_devfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in file_trace post_cache_trace block_trace; do
+for i in file_trace post_cache_trace; do
     major=`cat /proc/devices | grep $i | awk '{print $1}'`
     if [ -z "$major" ]; then
         echo "Couldn't find major for $i. is it loaded?"
