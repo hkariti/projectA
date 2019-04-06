67aea6ecfe219c7a9d9d246a0fe6f358b9eae2d7
Hagai Kariti <hkariti@gmail.com>
2019-04-06 14:17:37 +0300

    Add random read/write tests

---
+++ benchmarks/random_read.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import sys
+import random
+import os
+
+DEFAULT_SIZE = 1000
+BLOCK_SIZE = 512
+
+file_name = None
+block_count = None
+try:
+    file_name = sys.argv[1]
+    block_count = int(sys.argv[2])
+except:
+    print(f"Usage: {sys.argv[0]} FILE_NAME BLOCK_COUNT")
+    print(f"Read BLOCK_COUNT blocks (size={BLOCK_SIZE}) at random offsets from FILE_NAME")
+    sys.exit(1)
+
+print(f"Opening {file_name}")
+stat_file = os.stat(file_name)
+src_size = stat_file.st_size / BLOCK_SIZE
+if not src_size:
+    print(f"{file_name}: couldn't figure out size. Using default value of {DEFAULT_SIZE} blocks")
+    src_size = DEFAULT_SIZE
+src_file = open(file_name, 'rb')
+
+print(f"Reading {block_count} blocks in file with {src_size} blocks")
+for i in range(block_count):
+    block_nr = random.randint(0, src_size)
+    src_file.seek(block_nr * BLOCK_SIZE)
+    src_file.read(BLOCK_SIZE)
